d7a0b4af6ddc0b031733ed69f88de79e7be39246
syooj <syooj0728@gmail.com>
2023-01-24 14:49:32 +0900

    [FE] feat : Login google button test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 import './App.css';
 
 function App() {
+  const [ user, setUser ] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+  
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "935332498387-3esk9mek4urtfr27849s97ekvqk6haro.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+    
+    google.accounts.id.prompt();
+  }, []);
+  // 유저 없을 때 : signin 버튼
+  // 유저 있을 때 : logout 버튼
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      {Object.keys(user).length !== 0 &&
+        <button onClick={(event) => handleSignOut(event)}>SignOut</button>
+      }
+      { user &&
+        <div>
+          <img src={user.picture} alt=""></img>
+          <h3>{user.name}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      }
     </div>
   );
 }
---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const [isEmail,setIsEmail] = useState(undefined);
+    const [isPassword,setIsPassword] = useState(undefined);
+    
+    const [msgEmail,setMsgEmail] = useState("");
+    const [msgPassword,setMsgPassword] = useState("");
+
+    const emailChangeHandler = (e) =>{
+        const curEmail = e.target.value;
+        const emailValidCheck = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+        setEmail(curEmail);
+        
+        if(!emailValidCheck.test(curEmail)){
+            setIsEmail(false);
+            setMsgEmail("이메일 형식이 알맞지 않습니다.");
+        }else{
+            setIsEmail(true);
+            setMsgEmail("");
+        } 
+    }
+
+    const passwordChangeHandler = (e) =>{
+        const curPassword = e.target.value;
+        setPassword(curPassword);
+        const passwordValidCheck = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{9,16}$/;
+        if(!passwordValidCheck.test(curPassword)){
+            setIsPassword(false);
+            setMsgPassword("특수문자, 영어, 숫자를 포함한 9-16사이의 값을 입력하세요");
+        }else{
+            setIsPassword(true);
+            setMsgPassword("");
+        } 
+    }
+    
+    const testLogin = (body)=>{
+        console.log(body);
+        axios.post("/login", body ).then(res=>{
+            console.log(res.data);
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+
+    const onSubmitHandler = (e)=>{
+        e.preventDefault();
+        testLogin({email: email, password: password})
+    }
+
+    return (
+        <div className='Login'> 
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" value={email} id="email" onChange={emailChangeHandler}/>
+                <p>{msgEmail}</p>
+                </div>
+
+                <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" value={password} id="password" onChange={passwordChangeHandler}/>
+                <p>{msgPassword}</p>
+                </div>
+
+                {(isEmail && isPassword)? <button>로그인</button>: <button disabled={true}>로그인</button> }
+            </form>
+        </div>
+    );
+};
+
+export default Login;
