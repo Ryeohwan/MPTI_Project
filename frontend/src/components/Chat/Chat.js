407b8a474ba3b616045f9295db960f4e5edbd14a
YoonDongGeun <ehdrmsdl9999@naver.com>
2023-01-26 00:19:15 +0900

    [FE] fix:chat_send_bug

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -9,8 +9,9 @@ let stompClient = Stomp.over(socket);
 let channelId= '12';
 
 export default function Chat(){
+    const [roomlist, setroomlist] = useState([{channelId:1, userName:'Kim'}, {channelId:2, userName:'Jung'}])
     const [chatlist, setchatlist] = useState([]);
-    
+    console.log(chatlist)
     useEffect( () => {
         console.log(`연결 시도할 서버 : ${serverurl}`)
         stompClient.connect(
@@ -18,16 +19,16 @@ export default function Chat(){
             (frame) => {
 
                 console.log('연결했습니다', frame)
-                stompClient.subscribe("/send/12", (message) => {
+                stompClient.subscribe("/send/1", (message) => {
                     console.log("구독하고 받은 메시지",message)
-                    setchatlist([...chatlist,JSON.parse(message.body)])
+                    setchatlist([...chatlist, JSON.parse(message.body)])
                 })
             },
             (error) => {
                 console.log(error)
                 stompClient.connected = false
             }
-)
+    )
         
     }, [])
 
@@ -39,11 +40,11 @@ function handleSubmit(event) {
     console.log(event.target['0'].value)
     if (stompClient && stompClient.connected){
         const msg = {
-            'channelId': '12',
+            'channelId': '1',
             'userName': window.location.host,
             'content': event.target['0'].value
         }
-        stompClient.send("/chat/receive", JSON.stringify(msg),{})
+        stompClient.send("/chat/receive", {}, JSON.stringify(msg))
     }
     // console.log(input.value);
 }
@@ -59,6 +60,8 @@ function handleSubmit(event) {
                     </div>
                     <input className={styles.chat_search_bar} placeholder='사용자 검색'></input>
                 </div>
+
+
                 <div className={styles.chat_temp}>
                     <div id='message_box'>
                         {chatlist.map((chat) => {
@@ -66,14 +69,29 @@ function handleSubmit(event) {
                         })
                         }
                     </div>
+
+                    <div id={styles.room_list}>
+                        {roomlist.map((room) => {
+                            return (<div className={styles.room_box} onClick={() => {console.log('방 이동')}}>{room.channelId}번 방, {room.userName}님</div>)
+                        })}
+
+                    </div>
+
+
+
+
                     <div>
                         <form id='message_form' method='submit' onSubmit={(event)=>{handleSubmit(event)}}>
                             <input id='message_input' className={styles.chat_input}></input>
                             <button>Send</button>
                         </form>
                     </div>
-                    </div>
                 </div>
+
+
+
+
             </div>
+        </div>
     )
 }
