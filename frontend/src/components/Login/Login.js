d9c578dc53e96bcdd7e03490564436b5eb1d83c2
gksrud316 <gksrud316@naver.com>
2023-01-25 12:52:10 +0900

    [FE] test: Login state 관리 테스트

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./Login.module.css"
 
 const Login = () => {
+
+    
 const [email,setEmail] = useState("");
 const [password,setPassword] = useState("");
 
@@ -12,19 +14,19 @@ const [isPassword,setIsPassword] = useState(undefined);
 const [msgEmail,setMsgEmail] = useState("");
 const [msgPassword,setMsgPassword] = useState("");
 
+
 const emailChangeHandler = (e) =>{
     const curEmail = e.target.value;
     const emailValidCheck = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
     setEmail(curEmail);
-    
     if(!emailValidCheck.test(curEmail)){
+        //console.log(email);
         setIsEmail(false);
         setMsgEmail("이메일 형식이 알맞지 않습니다.");
     }else{
         setIsEmail(true);
         setMsgEmail("");
     }
-
 }
 
 const passwordChangeHandler = (e) =>{
@@ -53,6 +55,8 @@ const onSubmitHandler = (e)=>{
     e.preventDefault();
     testLogin({email: email, password: password})
 }
+
+
 
 return (
     <div className={styles.Login}> 
