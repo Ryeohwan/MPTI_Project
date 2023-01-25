1063b7ef68cade5b9f55f6e616fda5df22c58e09
YoonDongGeun <ehdrmsdl9999@naver.com>
2023-01-25 14:42:52 +0900

    [FE] type:repush

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const abc = (e) => {
-  (console.log(e)).then(() => {
-    console.log(1)
-  })
-}
-abc()
 root.render(
   <React.StrictMode>
     <App />
