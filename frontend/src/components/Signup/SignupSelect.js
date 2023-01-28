7c92fa2fef30f19a12ef2cbf78bf25a68f103676
정원철 <gksrud316@naver.com>
2023-01-28 17:14:16 +0900

    [FE] feat: 회원/트레이너 회원가입 선택 페이지 layout 구성

--- frontend/src/components/Signup/SignupSelect.js
+++ frontend/src/components/Signup/SignupSelect.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styles from "./SignupSelect.module.css"
+
 const SignupSelect = () => {
+    const navigate=useNavigate();
+    
     return (
         <div className={styles.SignupSelect}>
         
@@ -13,14 +17,14 @@ const SignupSelect = () => {
             <div className={styles.select_title}>회원가입</div>
             <div className={styles.select_box}>
                     <div className={styles.select_list}>
-                        <div className={styles.select_item}><span id='left'>&#9654;</span>고객</div>
-                        <div className={styles.select_item}><span id="right">&#9654;</span>트레이너</div>
+                        <div className={styles.select_item1}><span className={styles.select_square1} >&#9654;</span>고객</div>
+                        <div className={styles.select_item2}><span className={styles.select_square2}>&#9654;</span>트레이너</div>
                     </div>
             </div>
         </div>
 
         <div className={styles.select_btn_box}>
-           <button className={styles.select_btn}>뒤로가기</button>
+           <button className={styles.select_btn} onClick={()=>navigate(-1)}>뒤로가기</button>
         </div>
 
 
