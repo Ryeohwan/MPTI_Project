a3859c73b3e5ddb5d540137a647fa95fe288dbcd
syooj <syooj0728@gmail.com>
2023-01-28 16:28:09 +0900

    [FE] feat : Add calendar comp ClientMyReservation.js 진행중

--- frontend/src/pages/Client/ClientMyReservation.js
+++ frontend/src/pages/Client/ClientMyReservation.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import styles from './ClientMyReservation.module.css';
-import TopTitle from '../../components/Common/TopTitle'
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "./ClientMyReservation.module.css";
+import TopTitle from "../../components/Common/TopTitle";
+
 const ClientMyReservation = () => {
-    return (
-        <div className={styles.ClientMyReservation}>
-            <TopTitle title='예약하기▼' content='여러분과 딱 맞는 트레이너를 찾아보세요!'/>
-        </div>
-    );
+  const [value, onChange] = useState(new Date());
+  return (
+    <>
+      <div className={styles.ClientMyReservation}>
+        <TopTitle
+          title="예약하기▼"
+          content="여러분과 딱 맞는 트레이너를 찾아보세요!"
+        />
+      </div>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        className={styles.custom_calendar}
+        formatDay={(locale, date) =>
+          date.toLocaleString("en", { day: "numeric" })
+        }
+      />
+    </>
+  );
 };
 
 export default ClientMyReservation;
