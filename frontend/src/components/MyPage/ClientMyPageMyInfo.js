ea11881974a9a6bd28a897b26ffabd0a6bc81c98
YoonDongGeun <ehdrmsdl9999@naver.com>
2023-01-29 10:11:19 +0900

    fix:client/mypage/info

--- frontend/src/components/MyPage/ClientMyPageMyInfo.js
+++ frontend/src/components/MyPage/ClientMyPageMyInfo.js
@@ -9,7 +9,7 @@ const ClientMyPageMyInfo = () => {
         <div className={styles.container}> 
             <div className={styles.title}>내 개인정보</div>
             {edit?
-            <form className={styles.out_box} method='POST' onSubmit={(e) => {e.preventDefault(); console.log('a'); setEdit(false);}}>
+            <form className={styles.out_box} method='PUT' onSubmit={(e) => {e.preventDefault(); console.log('a'); setEdit(false);}}>
                 <div className={styles.in_box}>
                     <div className={styles.in_box_title}>✔ 인적사항</div>
                     {Object.values(get_info).map((value, index)=>
@@ -20,7 +20,9 @@ const ClientMyPageMyInfo = () => {
                 <div className={styles.edit_btn_box}><button className={styles.edit_btn} type='submit'>완료✔</button></div>
                 
             </form>
+                // edit 상태면 위의 양식을 출력 
             :
+                // edit 상태가 아니면 아래 양식을 출력
             <div className={styles.out_box}>
                 <div className={styles.in_box}>
                     <div className={styles.in_box_title}>✔ 인적사항</div>
