faa2487ce5a2a23b086c1af7c98547d2b2d3440d
서유진 <syooj0728@gmail.com>
2023-01-31 23:59:58 +0900

    Revert "Merge branch 'feature/FE/clientmyreservation/searchdatetime' into 'main'"
    This reverts merge request !7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
-// 사용 모듈or라이브러리
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter> 
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
