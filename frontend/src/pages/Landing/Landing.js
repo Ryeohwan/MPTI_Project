34b803e39bd3cb16fe12dfd3d2d67966183f5354
gksrud316 <gksrud316@naver.com>
2023-01-31 17:15:19 +0900

    [FE] style: 랜딩페이지 수정중

---
+++ frontend/src/pages/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Landing.module.css';
+const Landing = () => {
+    return (
+        <div className={styles.Landing}>
+          <div className={styles.logo}>MPTI</div>
+        </div>
+    );
+};
+
+export default Landing;
