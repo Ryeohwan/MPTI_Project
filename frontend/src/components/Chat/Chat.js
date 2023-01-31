33e1df7be4913752574858662864b49726d78d54
YoonDongGeun <ehdrmsdl9999@naver.com>
2023-02-01 01:20:54 +0900

    [FE] fix:bugfix

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -3,7 +3,8 @@ import styles from './Chat.module.css'
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import axios from 'axios';
-
+import RoomList from './RoomList';
+import ChatList from './ChatList';
 
 export default function Chat({chaton, turnoffchat}){
     const [roomnum, setRoomNum] = useState(undefined);
@@ -18,7 +19,6 @@ export default function Chat({chaton, turnoffchat}){
     const serverurl = 'http://i8a803.p.ssafy.io:8005'
     const socket = useRef(); // 소켓 서버
     const stompClient = useRef(); // stompe 서버
-
     useEffect(() => {
         if(chaton === false){
             document.getElementById('chat').style.display='none'
@@ -27,16 +27,17 @@ export default function Chat({chaton, turnoffchat}){
             }
         }
         else{
-
             socket.current = new SockJS(serverurl)
             stompClient.current= Stomp.over(socket.current)
-            // 채팅창 오픈
             document.getElementById('chat').style.display='flex'
-            // 소켓 서버 연결
             connect_socket_server();
         }
     },[chaton])
 
+    useEffect(() => {
+        const scroll = document.getElementById('chat_scroll')
+        scroll.scrollTop = scroll.scrollHeight
+    }, [roomnum])
     
     // 소켓 서버 연결
     function connect_socket_server() {
@@ -67,7 +68,16 @@ export default function Chat({chaton, turnoffchat}){
         // 소켓에 send했을때, 구독한 이곳에서 응답받고 이 함수를 실행. message는 응답 받은 메시지
         stompClient.current.subscribe(`/send/${room_id}`, (message) => {
             const msg= JSON.parse(message.body)
-            console.log('받은 메시지 :',msg)
+            console.log(stompClient.current)
+            console.log('받은 메시지 :',msg);
+            msg.year = new Date().getFullYear();
+            msg.month = new Date().getMonth()+1;
+            msg.day = new Date().getDate();
+            msg.hour = new Date().getHours();
+            msg.minute = new Date().getMinutes();
+            msg.newmessage = true;
+            msg.me=name===msg.userName?true:false;
+            console.log(msg)
             setchatlist((prev) =>([...prev, msg]))
         }, {id : room_id})
     }
@@ -109,24 +119,68 @@ export default function Chat({chaton, turnoffchat}){
     // 채팅방 목록 가져오기 (채팅 룸 id필요)
     function getRoomList() {
         axios.get(serverurl+`/chat/load/list/${userid}/${role}`).then((res) => {
+
             setRoomList(res.data)
         })
     }
 
     // 채팅방 목록 보여주기
     function showRoomList() {
+        //채팅방 목록 출력
+        
         return(
             roomlist.map((room, index) => {
-                return <div className={styles.room_box} key={index} onClick={(event) => {enter_chat_room(room.channelId); console.log(`${room.channelId}방 이동`)}}>{room.yourId}님 과의 대화, 내용:{room.content}</div>
+                return <div key={`room${room.channelId}`}>
+                        <RoomList content={room.content} user={room.yourId} channelId={room.channelId} enter_chat_room={enter_chat_room} />
+                        </div>
             })
         )
     }
     // 채팅 내역 가져오기
     async function getChatList(room_id) {
         axios.get(serverurl+'/chat/load/'+ room_id).then((res) => {
+            res.data.map((Adata) => {
+                if(Adata.writer===name)Adata.me=true; else Adata.me=false;
+                const [date, time]=Adata.createdDate.split('T');
+                [Adata.year, Adata.month, Adata.day] = date.split('-');
+                [Adata.hour, Adata.minute] = time.split(':');
+                Adata.hour = (parseInt(Adata.hour)+7)%24
+            })
             setchatlist(res.data)
         })
     }
+
+    // 상대방과 채팅 내역 보여주기
+    function showChatList() {
+        if(chatlist){
+            return(
+                
+                chatlist.map((chat, index) => {
+                    if(index && chat.day===chatlist[index-1].day && chat.month === chatlist[index-1].month && chat.year === chatlist[index-1].year){ 
+                        return  <div key={`chat${chat.id}`}>
+                                    <ChatList {...chat} />
+                                </div>
+                    }
+                    // 처음 채팅, 날짜가 달라지는 채팅은 날짜를 추가해준다.
+                    if(chat.newmessage){
+                        return  <div key={`chat${chat.id}`}>
+                                    <ChatList {...chat} />
+                                </div>
+                    }
+                    return  <div key={`chat${chat.id}`}>
+                                <div className={styles.chat_list_day}>{chat.year}년 {chat.month}월 {chat.day}일</div>
+                                <ChatList {...chat} />
+                            </div>
+                })
+                
+            )
+        }
+        else{
+            return <div>채팅방이 없습니다.</div>
+        }
+            
+    }
+
     // async function getChatList(target) {
     //     const room_id = await searchChannel(target)
     //     axios.get(serverurl+'/chat/load/'+ room_id).then((res) => {
@@ -134,14 +188,6 @@ export default function Chat({chaton, turnoffchat}){
     //         setchatlist(res.data)
     //     })
     // }
-    // 상대방과 채팅 내역 보여주기
-    function showChatList() {
-        return(
-            chatlist.map((chat, index) => {
-                return <div key={index}>{chat.content}</div>
-            })
-        )
-    }
 
 
     return(
@@ -149,36 +195,28 @@ export default function Chat({chaton, turnoffchat}){
             <div className={styles.modal_box}>
                 <div className={styles.title_search}>  
                     <div className={styles.chat_title_box}>
-                        {roomnum? [<div className={styles.chat_exit_btn} onClick={() => {leave_chat_room()}}>&lt;&nbsp;</div>,<div>{roomnum}번 방</div>]:[<div>&nbsp;&nbsp;</div>,<div>채팅방</div>]}
+                        {roomnum? [<div key='back' className={styles.chat_exit_btn} onClick={() => {leave_chat_room()}}>&lt;&nbsp;</div>,<div key='room_name'>{roomnum}번 방</div>]:[<div key='whitespace'>&nbsp;&nbsp;</div>,<div key='roomlist'>채팅방</div>]}
                         <div className={styles.chat_exit_btn} onClick={()=>{turnoffchat()}}>❌</div>
                     </div>
                     <input className={styles.chat_search_bar} placeholder='사용자 검색'></input>
                 </div>
 
 
-                <div className={styles.chat_temp}>
+                <div id='chat_scroll' className={styles.chat_temp}>
                     {
                     roomnum?
                         <div id={styles.message_box}>{showChatList()}</div>
                         :
                         <div id={styles.room_list}>{showRoomList()}</div>
                     }
-                    <div>
-                        <form className={styles.message_form} method='submit' onSubmit={(event)=>{handleSubmit(event)}}>
-                            <input id='message_input' className={styles.chat_input}></input>
-                            <button>Send</button>
-                        </form>
-                        {/* <div>사용자 번호</div>
-                        <input onChange={(e)=>setUserId(e.target.value)} defaultValue={userid}></input>
-                        <div> 상대방 번호</div>
-                        <input onChange={(e)=>setTarget(e.target.value)} defaultValue={target}></input>
-                        <div>Role</div>
-                        <input onChange={(e)=>setRole(e.target.value)} defaultValue={role}></input>
-                        <div>이름</div>
-                        <input onChange={(e)=>setName(e.target.value)} defaultValue={name}></input>
-                        <button onClick={() => getChatList(target)} >채팅방 접속</button> */}
-                    </div>
                 </div>
+                
+                        {roomnum?<form className={styles.message_form} method='submit' onSubmit={(event)=>{handleSubmit(event); event.target['0'].value=null; }}>
+                            <input id='message_input' className={styles.chat_input}></input>
+                            <button className={styles.input_button}>send</button>
+                        </form>:null}
+                        
+                
             </div>
         </div>
     )
