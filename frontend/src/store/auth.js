dc059008e49d3ff6834e96b701d46daa9e22bbbe
정원철 <gksrud316@naver.com>
2023-02-05 22:43:18 +0900

    [FE] feat: auth 설정, 로그인, 로그아웃, 중복체크 함수 설정

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,61 +1,98 @@
-import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { createSlice } from '@reduxjs/toolkit';
 
-
-const initialAuthState = {
-  isAuthenticated: false, 
+const initialState = {
+    isLoading: false,
+    isLoggedIn: false,
+    error: null,
+    isCheckMsg: ""
 };
-	
+
+
+
 const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-
-    google(state){
-
-    },
-    login(action) {
-    
-    axios.post("/api/auth/login", action.payload)
-      .then((res)=>{
-        console.log(res);
-        console.log(res.headers["authorization"])
-        console.log(res.headers["refresh-token"]);
-        localStorage.setItem("access_token", res.headers["authorization"]);
-        localStorage.setItem("refresh_token", res.headers["refresh-token"]);
+    name: 'auth',
+    initialState,
+    reducers: {
+        loginRequest: (state) => {
+            state.isLoading = true;
+        },
+        loginSuccess: (state) => {
+            state.isLoading = false;
+            state.isLoggedIn = true;
+        },
+        loginFailure: (state, action) => {
+            state.isLoading = false;
+            state.isLoggedIn = false;
+            state.error = action.payload;
+        },
+        logoutSuccess: (state) => {
+            state.isLoading = false;
+            state.isLoggedIn = false;
+        },
+        logoutFailure: (state, action) => {
+            state.isLoading = false;
+            state.isLoggedIn = false;
+            state.error = action.payload;
+        },signupRequest: (state) => {
+            state.isLoading = true;
+        },
+        signupSuccess: (state) => {
+            state.isLoading = false;
+        },
+        duplicateMsg: (state,action)=>{
+            state.isCheckMsg= action.payload;
+        }
         
-      }).catch ((err)=>{
-          console.log(err);
-   
-      })
-
-    
     },
-
-    logout(state) {
-
-      const accessToken = localStorage.getItem('access_token');
-      const refreshToken = localStorage.getItem('refresh_token');
-
-      axios.defaults.headers.common['authorization'] = accessToken;
-      axios.defaults.headers.common['refresh-token'] = refreshToken;
-
-      axios.post("/api/auth/logout").then((res)=>{
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        
-        console.log(res);
-        
-      }).catch((err)=>{
-        console.log(err);
-      })
-
-      state.isAuthenticated = false;
-    },
-  },
 });
 
-export const authActions = authSlice.actions;
 
+
+export const login = (email, password) => async (dispatch) => {
+        dispatch(authActions.loginRequest());
+    try {
+        const response = await axios.post("/api/auth/login", { email, password });
+        localStorage.setItem("access_token", response.headers["authorization"]);
+        localStorage.setItem("refresh_token", response.headers["refresh-token"]);
+        console.log(response);
+        console.log("로그인성공");
+        dispatch(authActions.loginSuccess());
+    } catch (error) {
+        dispatch(authActions.loginFailure(error));
+    }
+};
+
+export const logout = () => async(dispatch)=>{
+    try {
+        const accessToken = localStorage.getItem('access_token');
+        const refreshToken = localStorage.getItem('refresh_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('access_token');
+        axios.defaults.headers.common['authorization'] = accessToken;
+        axios.defaults.headers.common['refresh-token'] = refreshToken;
+        const response= await axios.post("/api/auth/logout");
+        console.log(response);
+        console.log("로그아웃 성공");
+        dispatch(authActions.logoutSuccess());
+    } catch (error) {
+        dispatch(authActions.logoutFailure(error));
+    }
+}
+
+export const duplicateCheck = (type,email) => async(dispatch)=>{
+    try {
+        const response= await axios.get(`/api/${type}/duplicate/${email}`);
+        console.log(response);
+        console.log("중복확인 성공");
+        dispatch(authActions.duplicateMsg("중복된 아이디가 없습니다."));
+    } catch (error) {
+        console.log(error.response)
+        dispatch(authActions.duplicateMsg("중복된 아이디가 있습니다."));
+    }
+}
+
+
+
+export const authActions = authSlice.actions;
 export default authSlice.reducer;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,7 +5,7 @@ import authReducer from './auth';
 
 
 const store = configureStore({
-  reducer: { counter: counterReducer, auth: authReducer },
+  reducer: { counter: counterReducer, auth: authReducer},
 });
 
 export default store;
