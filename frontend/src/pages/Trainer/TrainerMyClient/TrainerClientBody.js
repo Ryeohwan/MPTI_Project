cea7a03f3ddd6b01301e421fcf1cd2298b41a55a
gksrud316 <gksrud316@naver.com>
2023-02-13 13:38:28 +0900

    [FE] feat: 관리자 고객관리 페이징 처리

--- frontend/src/pages/Trainer/TrainerMyClient/TrainerClientBody.js
+++ frontend/src/pages/Trainer/TrainerMyClient/TrainerClientBody.js
@@ -10,14 +10,14 @@ const TrainerClientBody = (props) => {
   const [searchValue, setSearchValue] = useState(""); //검색어
   const [select, setSelect] = useState(1); //선택한 페이지
   const [targetClient, setTargetClient] = useState(undefined); //상세페이지 볼 클라이언트 id
-  const data = [
-    { id: 1, name: "정원철", gender: "남", age: 28, time: "14:00 - 15:00" },
-    { id: 2, name: "이예은", gender: "남", age: 28, time: "14:00 - 15:00" },
-    { id: 3, name: "윤동근", gender: "남", age: 28, time: "14:00 - 15:00" },
-    { id: 4, name: "서유진", gender: "남", age: 28, time: "14:00 - 15:00" },
-    { id: 5, name: "지선호", gender: "남", age: 28, time: "14:00 - 15:00" },
-    { id: 6, name: "안려환", gender: "남", age: 28, time: "14:00 - 15:00" },
-  ];
+  // const data = [
+  //   { id: 1, name: "정원철", gender: "남", age: 28, time: "14:00 - 15:00" },
+  //   { id: 2, name: "이예은", gender: "남", age: 28, time: "14:00 - 15:00" },
+  //   { id: 3, name: "윤동근", gender: "남", age: 28, time: "14:00 - 15:00" },
+  //   { id: 4, name: "서유진", gender: "남", age: 28, time: "14:00 - 15:00" },
+  //   { id: 5, name: "지선호", gender: "남", age: 28, time: "14:00 - 15:00" },
+  //   { id: 6, name: "안려환", gender: "남", age: 28, time: "14:00 - 15:00" },
+  // ];
   const [signupList, setSignupList] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage]= useState(0);
@@ -29,7 +29,7 @@ const TrainerClientBody = (props) => {
   useEffect(() => {
     
     axios
-      .post(`/api/user/userList/${3}`, { id: 1 })
+      .post(`/api/user/userList/${page-1}`, { id: 1 })
       .then((res) => {
 
         console.log(res);
@@ -49,7 +49,7 @@ const TrainerClientBody = (props) => {
     <div>
       <div className={styles.container2}>
         <ClientInfo
-          {...data.find((item) => item.email === targetClient)}
+          {...signupList.find((item) => item.email === targetClient)}
           setTargetClient={setTargetClient}
         />
       </div>
