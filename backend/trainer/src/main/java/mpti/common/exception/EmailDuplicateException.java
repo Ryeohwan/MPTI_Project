79c9779349a30cc9ec9532693ac79c5937343576
unknown <dodamond222@gmail.com>
2023-02-14 11:02:39 +0900

    [BE] feat : init project

--- backend/trainer/src/main/java/mpti/common/exception/EmailDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package mpti.common.exception;
-
-public class EmailDuplicateException extends RuntimeException {
-    public EmailDuplicateException(String email) {
-        super("This email already exist : " + email);
-    }
-}
